0a6d427075cf882642d592b67fd04b10c4dac26f
Abdul Ahad <abdul.ahad117266@gmail.com>
2024-08-06 17:55:04 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// // THE DESING RELATED CODES GOES HERE
+
+// let playlistCard = document.querySelectorAll(".card");
+// playlistCard.forEach(element => {
+//     let play = element.querySelector(".play");
+//     element.addEventListener("mouseover", () => {
+//       play.style.opacity = 1;
+//     }
+//     )
+// });
+
+async function getHref() {
+    let songsHrefs = [];
+    let div = document.createElement("div");
+    div.innerHTML = await getData();
+    let as = div.getElementsByTagName("a");
+    for (const a of as) {
+        if (a.href.endsWith(".mp3")) {
+            songsHrefs.push(a.href);
+        }
+    }
+    return songsHrefs
+}
+
+var currentSong = new Audio();
+// AI written functions are here
+function secondsToMMSS(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.round(seconds % 60); // Round the remaining seconds
+
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+// All the essential functions are here
+async function getData() {
+    let a = await fetch("songs");
+    let response = await a.text();
+    return response;
+}
+
+function playMusic(path, pause=false) {
+    currentSong.src = path;
+    if (!pause) {
+        currentSong.src = "/songs/" + path;
+        currentSong.play()
+        document.querySelector(".while-play-songName").innerHTML = path.replace(".mp3", "").replace("-", " - ");
+        play.src = "/media/svgs/songPause.svg";
+    }
+
+    else {
+        document.querySelector(".while-play-songName").innerHTML = path.replace("http://127.0.0.1:5500/songs/", "").replace("%20", " ").replace("-", " - ").replace(".mp3", "");
+    }
+}
+
+async function getName() {
+    let songNames = [];
+    let div = document.createElement("div");
+    div.innerHTML = await getData();
+    let as = div.getElementsByTagName("a");
+    for (const a of as) {
+        if (a.title.endsWith(".mp3")) {
+            let name = a.title.replace(".mp3", "");
+            songNames.push(name);
+        }
+    }
+    return songNames;
+}
+
+
+
+async function showName() {
+    let songNames = await getName();
+    for (const song of songNames) {
+        const [songName, artist] = song.split("-", 2);
+        let songList = document.body.querySelector(".song-list");
+        songList.innerHTML = songList.innerHTML +
+            `
+            <li class="flex">
+            <img src="media/svgs/tune.svg" class="music-icon" alt="music">
+            <div class="song-info">
+                <div class="name">${songName}</div>
+                <div class="artist">${artist}</div>
+            </div>
+            <div class="playNow">
+                <span>Play now</span>
+                <img src="media/svgs/play.svg" alt="">
+            </div>
+            </li>
+        `;
+
+    }
+    // the code below is making it possible to play the songs
+    let songsLI = document.querySelector(".song-list").getElementsByTagName("li");
+    Array.from(songsLI).forEach(element => {
+        element.addEventListener("click", () => {
+            playMusic(element.querySelector(".song-info").firstElementChild.innerHTML + "-" + element.querySelector(".artist").innerHTML + ".mp3");
+        }
+        )
+    });
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "/media/svgs/songPause.svg"
+        } else {
+            currentSong.pause();
+            play.src = "/media/svgs/playSong.svg"
+        }
+    }
+    )
+}
+
+async function main() {
+    currentSong.addEventListener("timeupdate", () => {
+        let status = document.querySelector(".status");
+        let currentTime = secondsToMMSS(currentSong.currentTime);
+        let duration = secondsToMMSS(currentSong.duration);
+        document.querySelector(".while-play-duration").innerHTML = `${currentTime}/${duration}`;
+        let left = (currentSong.currentTime/currentSong.duration)*100;
+        status.style.left = left+"%";
+    }
+    )
+
+
+    let link = await getHref()
+    playMusic(link[0], true)
+}
+
+main();
+showName();
+// playMusic(getHref()[0], true);
